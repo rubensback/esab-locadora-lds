a489152db2e3315047efdc285e61b15e5414c7d7
rubensback <rubensback@gmail.com>
2023-02-15 16:29:19 +0000

    feat: Initial configs and theme

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <div>Home</div>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
+
+export default App
